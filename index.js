f9e62a1cdaadc35ddf146002f7d032479850d43c
christopherwperry <christopherwperry@gmail.com>
2017-08-17 15:20:11 -0400

    form submission creates new todo if input has value and deletes todo if empty

--- index.js
+++ index.js
@@ -5,28 +5,45 @@ const expressValidator = require('express-validator');
 const port = 3000;
 const path = require('path');
 const app = express();
-const todos = [
-  "Wash the car"
-];
+
+let todo = [
+  "Wash the car",
+  "Take out trash",
+  "Pay Rent",
+  "Wash dog"
+]
+
+let completed = [
+  "Shower",
+  "Eat Breakfast",
+  "Exercise",
+  "Mail letters"
+]
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  res.render('index', {todo: todo, completed: completed});
 });
 
-app.get('/todo', function(req, res){
-  res.render('index');
+app.post("/", function (req, res) {
+  console.log(req.body.todo);
+  console.log(this.value);
+  if (req.body.todo){
+    todo.push(req.body.todo);
+    res.redirect('/');
+  } else {
+    todo.pop(this.value);
+    completed.push(this.value);
+    res.redirect('/');
+  }
 });
-
-app.post("/todo", function (req, res) {
-  todos.push(req.body.todo);
-  res.redirect('/todo');
-})
 
 app.listen(port, function(){
-  console.log("The server is running on port 3000")
+  console.log("The server is running on port 3000");
 });
